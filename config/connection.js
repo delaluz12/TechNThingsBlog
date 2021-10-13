2e8e947a14efbd7e2c2a11cd2180cb7c3b798864
delaluz12 <75863526+delaluz12@users.noreply.github.com>
2021-10-13 15:23:52 -0500

    heroku db management

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,24 @@
 const Sequelize = require('sequelize');
-require('dotenv').config();
+require('dotenv').config({
+  path: require('find-config')('.env')
+});
 
 let sequelize;
 
 //checks to see if session w/b initialized from db in cloud server on heroku or local machine
 if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-  } else {
-    sequelize = new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306
-      }
-    );
-  }
-  
-  module.exports = sequelize;
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
+
+module.exports = sequelize;
